72a9c83d7f019183977fa87f6ea2c945feb14ab9
giacintocarlucci <Giacinto1996@live.it>
2021-09-07 19:38:56 +0200

    [fix] cleaned extra-monitor.sh

--- sh/extra-monitor.sh
+++ sh/extra-monitor.sh
@@ -4,4 +4,4 @@
 main="DP-1"
 extra="DP-2"
 
-xrandr --output "$extra" --mode 1920x1080 --rotate right && xrandr --output "$main" --auto --output "$extra" --auto --rotate right --left-of "$main"
+xrandr --output "$main" --auto --output "$extra" --auto --rotate right --left-of "$main"
