3b6ad302563b745b1225c99104103d53ec1e5ae0
giacintocarlucci <Giacinto1996@live.it>
2021-09-08 12:04:34 +0200

    [doc] extra-monitor.sh comments on how to use

--- sh/extra-monitor.sh
+++ sh/extra-monitor.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # Add a second vertical monitor to the left using xrandr
+# In order to launch it after login, copy this script in
+# /etc/local.d/ folder, and give it execution permissions.
 
 main="DP-1"
 extra="DP-2"
 
-xrandr --output "$main" --auto --output "$extra" --auto --rotate right --left-of "$main"
+xrandr --output "$main" --primary --auto --output "$extra" --auto --rotate right --left-of "$main"
